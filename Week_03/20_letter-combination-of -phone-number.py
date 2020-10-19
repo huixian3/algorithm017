21cc67772ebb6e9e4a290d9e579f7ac9462e7990
zhanhuixian <zhanhuixian@meituan.com>
2020-10-19 19:27:55 +0800

    ancester

--- Week_03/20_letter-combination-of -phone-number.py
+++ Week_03/20_letter-combination-of -phone-number.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: List[str]
         """
         # 暴力方法，n层循环
-        # 递归
+        # 递归 回溯
         result = []
         dic = {'2':'abc','3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
         def travese(l, n, res):
---
+++ Week_03/21-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,16 @@
+def lowestCommonAncestor(self, root, p, q):
+    """
+    :type root: TreeNode
+    :type p: TreeNode
+    :type q: TreeNode
+    :rtype: TreeNode
+    """
+    if not root or root == p or root == q:
+        return root
+    left = self.lowestCommonAncestor(root.left, p, q)
+    right = self.lowestCommonAncestor(root.right, p, q)
+    if not left:
+        return right
+    if not right:
+        return left
+    return root
